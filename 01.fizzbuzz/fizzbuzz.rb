4c3502b4196dca8ed362a6dda6119d97bb3a5105
wak9102 <k.wak91021@gmail.com>
2021-02-15 22:55:35 +0900

    fizzbuzz.rbのインデントを修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,15 +2,15 @@
 
 int = 1
 while int <= 100 do
-	if int % (3 * 5) == 0
-		puts "FizzBuzz"
-	elsif int % 3 == 0
-		puts "Fizz"
-	elsif int % 5 == 0
-		puts "Buzz"
-	else
-		puts int
-	end
-	int += 1
+  if int % (3 * 5) == 0
+    puts "FizzBuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  else
+    puts int
+  end
+int += 1
 end
 
