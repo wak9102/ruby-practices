51ff033599d546d72f5c28b2e7293fba14b1d0d5
wak9102 <k.wak91021@gmail.com>
2021-06-09 19:10:59 +0900

    11フレーム目以降を10フレーム目にまとめず集計する処理に変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,67 +1,63 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# 引数のスコアを配列に格納する
+# スコアを配列に格納する
 scores = ARGV[0].split(',')
 
-# スコアの配列の要素をint型にする
-# ストライクは10に置換する
-# 9フレーム目（17投目）までのストライクの次には0を追加する
+# スコア配列の要素をintにする
+# ストライクの場合は10に置換する
 shots = []
-
 scores.each do |score|
   if score == 'X'
     shots << 10
-    if shots.count <= 17
-      shots << 0
-    end
+    shots << 0
   else
     shots << score.to_i
   end
 end
 
-# スコアの配列をフレームごとの配列にする
-# 1フレーム2投ずつ格納して11個目の要素があれば10個目にまとめる
+# スコア配列を2投ずつフレームごとの配列に格納する
 frames = []
-
 shots.each_slice(2) { |shot| frames << shot }
-if frames[10]
-  frames[9].push(*frames[10])
-  frames.delete_at(10)
-end
 
-# フレームごとに得点を加算する
-# 前フレームがスペアの場合は、1投目を2倍加算する
-# 前フレームと前々フレームがストライクの場合は、1投目を3倍、2投目を2倍で加算する
-# 前フレームだけがストライクの場合は、1投目を2倍、2投目を2倍で加算する
-
+# フレームごとにスコアを集計する
+#   10フレーム以前は
+#     前フレームがスペアの場合、1投目を2倍加算する
+#     前フレームと前々フレームがストライクの場合、1投目を3倍、2投目を2倍加算する
+#     前フレームだけストライクの場合、1投目を2倍、2投目を2倍加算する
+#   11フレーム以降は
+#     前フレームと前々フレームがストライクの場合、11フレームのみ1投目を2倍加算する
 points = 0
-spare_or_strike_last_time = nil # 前フレームがスペアかストライクだったかを格納
-spare_or_strike_the_time_before_last = nil # 前々フレームがスペアかストライクだったかを格納
+spare_or_strike_last_time = nil # 前フレームがスペアかストライクかを格納する
+spare_or_strike_the_time_before_last = nil # 前々フレームがスペアかストライクかを格納する
 
-frames.each do |first, second|
-  # 今回のフレームの得点を算出する
-  case spare_or_strike_last_time
-  when :spare then points += (first * 2 + second)
-  when :strike
-    points += if spare_or_strike_the_time_before_last == :strike
-                (first * 3 + second * 2)
-              else
-                (first * 2 + second * 2)
-              end
-  else points += (first + second)
+frames.each.with_index(1) do |shot, index|
+  if index <= 10
+    case spare_or_strike_last_time
+    when :spare then points += (shot[0] * 2 + shot[1])
+    when :strike
+      points += if spare_or_strike_the_time_before_last == :strike
+                  (shot[0] * 3 + shot[1] * 2)
+                else
+                  (shot[0] * 2 + shot[1] * 2)
+                end
+    else points += shot.sum
+    end
+
+    # スペアとストライクの状態を更新する
+    spare_or_strike_the_time_before_last = spare_or_strike_last_time
+    spare_or_strike_last_time = if shot[0] == 10
+                                  :strike
+                                elsif shot[0] + shot[1] == 10
+                                  :spare
+                                end
+
+  elsif index == 11 and spare_or_strike_last_time == :strike and spare_or_strike_the_time_before_last == :strike
+    points += (shot[0] * 2 + shot[1])
+
+  else
+    points += shot.sum
   end
-
-  # スペアとストライクの状態を更新する
-  spare_or_strike_the_time_before_last = spare_or_strike_last_time
-  spare_or_strike_last_time = if first == 10
-                                :strike
-                              elsif first + second == 10
-                                :spare
-                              end
 end
-
-# 10フレーム目に3投目があれば加算する
-points += frames[9][2] if frames[9][2]
 
 puts points
