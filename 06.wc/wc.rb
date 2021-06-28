85901e02e17dbbd1634c593fc733c105d19ffb7c
wak9102 <k.wak91021@gmail.com>
2021-06-28 18:26:10 +0900

    wc.rbにカウント結果の出力機能を追加
    取得したカウント結果をオプションと引数の数に応じて分岐して出力する機能を追加しました。

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -2,7 +2,7 @@
 
 require 'optparse'
 
-# メソッド：テキストのline、word、byteをカウント
+# 文字列からline、word、byteをカウントするメソッド
 def wc_count(text)
   {
     :line => text.count("\n"),
@@ -10,7 +10,6 @@ def wc_count(text)
     :byte => text.bytesize
   }
 end
-
 
 # コマンドラインオプションを配列に格納
 options = []
@@ -21,38 +20,39 @@ opt.parse!(ARGV)
 # コマンドライン引数を配列に格納
 arg_files = ARGV
 
-# 引数のファイル情報を配列に格納
+# 引数のファイルごとのカウント結果をhashに格納し配列に追加
 counted_results = []
-counted_result_original = {}
 
 if  arg_files.size > 0
   arg_files.each do |file|
-    counted_result = counted_result_original.dup
+    counted_result = Hash.new
     counted_result[:name] = file
-    text = File.open(file, "r") { |f| f.read }
-    counted_result.merge!(wc_count(text))
+    file_text = File.open(file, "r") { |f| f.read }
+    counted_result.merge!(wc_count(file_text))
     counted_results << counted_result
   end
-else # コマンドライン引数がない場合は標準入力を参照
-  counted_result = counted_result_original.dup
+else # 引数がない場合は標準入力のカウント結果をhashに格納し配列に追加
+  counted_result = Hash.new
   counted_result[:name] = :stdin
-  text = $stdin.readlines.join
-  counted_result.merge!(wc_count(text))
+  stdin_text = $stdin.readlines.join
+  counted_result.merge!(wc_count(stdin_text))
   counted_results << counted_result
 end
 
+# 要素ごとに結果を出力
+counted_results.each do |result|
+  print result[:line].to_s.rjust(8)
+  print result[:word].to_s.rjust(8) unless options.include?(:line)
+  print result[:byte].to_s.rjust(8) unless options.include?(:line)
+  print " #{result[:name]}" unless result[:name] == :stdin
+  print "\n"
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 引数が2つ以上ある場合はtotalも出力
+if arg_files.size >= 2
+  print counted_results.inject(0) { |sum, result| sum + result[:line] }.to_s.rjust(8)
+  print counted_results.inject(0) { |sum, result| sum + result[:word] }.to_s.rjust(8) unless options.include?(:line)
+  print counted_results.inject(0) { |sum, result| sum + result[:byte] }.to_s.rjust(8) unless options.include?(:line)
+  print " total\n"
+end
 
