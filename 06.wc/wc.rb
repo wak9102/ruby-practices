4c13ec513db54eda4d7023531179fab74c4506e9
wak9102 <k.wak91021@gmail.com>
2021-07-02 18:43:06 +0900

    wc.rbの一部をリファクタリング
    hashの新規作成、unless修飾子の箇所を変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -26,15 +26,13 @@ counted_results = []
 
 if arg_files.size.positive?
   arg_files.each do |file|
-    counted_result = {}
-    counted_result[:name] = file
+    counted_result = { name: file }
     file_text = File.open(file, 'r', &:read)
     counted_result.merge!(wc_count(file_text))
     counted_results << counted_result
   end
 else # 引数がない場合は標準入力のカウント結果をhashに格納し配列に追加
-  counted_result = {}
-  counted_result[:name] = :stdin
+  counted_result = { name: :stdin }
   stdin_text = $stdin.readlines.join
   counted_result.merge!(wc_count(stdin_text))
   counted_results << counted_result
@@ -43,16 +41,22 @@ end
 # 要素ごとに結果を出力
 counted_results.each do |result|
   print result[:line].to_s.rjust(8)
-  print result[:word].to_s.rjust(8) unless options.include?(:lines)
-  print result[:byte].to_s.rjust(8) unless options.include?(:lines)
-  print " #{result[:name]}" unless result[:name] == :stdin
+  unless options.include?(:lines)
+    print result[:word].to_s.rjust(8)
+    print result[:byte].to_s.rjust(8)
+  end
+  unless result[:name] == :stdin
+    print " #{result[:name]}"
+  end
   print "\n"
 end
 
 # 引数が2つ以上ある場合はtotalも出力
 if arg_files.size >= 2
   print counted_results.inject(0) { |sum, result| sum + result[:line] }.to_s.rjust(8)
-  print counted_results.inject(0) { |sum, result| sum + result[:word] }.to_s.rjust(8) unless options.include?(:lines)
-  print counted_results.inject(0) { |sum, result| sum + result[:byte] }.to_s.rjust(8) unless options.include?(:lines)
+  unless options.include?(:lines)
+    print counted_results.inject(0) { |sum, result| sum + result[:word] }.to_s.rjust(8)
+    print counted_results.inject(0) { |sum, result| sum + result[:byte] }.to_s.rjust(8)
+  end
   print " total\n"
 end
